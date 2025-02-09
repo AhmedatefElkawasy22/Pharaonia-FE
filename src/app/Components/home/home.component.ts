1e5d7183530b63c7b16f5715de298a379819b505
Rawanbahaa142 <rwanalrawy162@gmail.com>
2025-02-09 21:27:36 +0200

    for pull

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DestinationServiceService } from '../../Services/destination.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  destinations: any[] = [];
+  isLoading: boolean = true;
+  errorMessage: string = '';
 
+ constructor(
+    //private _DestinationService: DestinationServiceService,
+
+  ) {}
+  ngOnInit(): void {
+   // this.fetchDestinations();
+  }
+
+//   fetchDestinations(): void {
+//     this.destinationService.getDestinations().subscribe({
+//       next: (data) => {
+//         this.destinations = data;
+
+//       },
+//       error: (err) => {
+//         this.errorMessage = 'Failed to load destinations. Please try again.';
+
+//       },
+//     });
+//   }
+//
 }
