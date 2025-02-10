b17242dcbe5f447edcb81d0fcdb72622ab2cbd60
AhmedatefElkawasy22 <elkawasyahmed@gmail.com>
2025-02-10 02:04:09 +0200

    call all apis aboutusService

--- src/app/Services/aboutUs/about-us.service.ts
+++ src/app/Services/aboutUs/about-us.service.ts
@@ -1,17 +1,55 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environment/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { AccountServiceService } from '../account/account-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutUsService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient,private _accountServices:AccountServiceService) { }
 
   GetAboutUs(): Observable<any> {
     return this._http.get<any>(`${environment.BaseURL}/Get-AboutUS` );
+  }
+
+  updateAboutUs(data: any): Observable<any> {
+    const token = localStorage.getItem("token");
+    if (token && this._accountServices.isTokenValid()) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+      return this._http.put(`${environment.BaseURL}/update-AboutUs`, data,{responseType: 'text' , headers});
+    } else {
+      this._accountServices.logout();
+      return of(null);
+    }
+  }
+  AddAboutUs(data: any): Observable<any> {
+    const token = localStorage.getItem("token");
+    if (token && this._accountServices.isTokenValid()) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+      return this._http.post(`${environment.BaseURL}/Add-AboutUs`, data,{responseType: 'text' , headers});
+    } else {
+      this._accountServices.logout();
+      return of(null);
+    }
+  }
+  DeleteAboutUs(): Observable<any> {
+    const token = localStorage.getItem("token");
+    if (token && this._accountServices.isTokenValid()) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+      return this._http.delete(`${environment.BaseURL}/Delete-AboutUs`,{responseType: 'text' , headers});
+    } else {
+      this._accountServices.logout();
+      return of(null);
+    }
   }
 
 }
