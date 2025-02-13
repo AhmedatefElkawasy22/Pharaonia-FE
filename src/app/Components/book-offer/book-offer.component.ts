cad98439e318b0fcec0c541833768eaedc68bc30
AhmedatefElkawasy22 <elkawasyahmed@gmail.com>
2025-02-13 07:49:41 +0200

    add images to destination & updated images is done

---
+++ src/app/Components/book-offer/book-offer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-offer',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-offer.component.html',
+  styleUrl: './book-offer.component.css'
+})
+export class BookOfferComponent {
+
+}
